85f7018044a1b31dcc7d6b5bba6d9f1bf777218a
Manuel Gutierrez Casares <91099270+ManuelGutierrezCasares@users.noreply.github.com>
2023-11-17 13:13:35 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# odin-form-validator
-Form Validation with JavaScript
+# Form Validator with JavaScript Validation API
+
+Form Validation with vanilla JavaScript
+
+Here's a simple Form Validator made with vanilla Javascript.
+
+Information:
+- Every input is validated on the fly
+- When form is validated completely, you can submit to get a '¡High Five!' alert.
+- Bootstrap v5.3 is used for styling
+
+Requirements:
+- Internet browser with JavaScript Engine.
---
+++ main.js
@@ -0,0 +1,103 @@
+(function() {
+  const email = document.getElementById('form-email');
+  const country = document.getElementById('form-country');
+  const zipCode = document.getElementById('form-zip-code');
+  const password = document.getElementById('form-password');
+  const passwordConfirmation = document.getElementById('form-password-confirmation');
+  const submit = document.getElementById('form-submit');
+
+  submit.addEventListener('click', function (e) {
+    if (email.validity.valid && country.validity.valid && zipCode.validity.valid && password.validity.valid && passwordConfirmation.validity.valid){
+      e.preventDefault();
+      form.reset();
+      alert('¡HIGH FIVE!');
+    }
+  })
+
+  email.addEventListener('input', function (e) {
+    emailValidator(e);
+  })
+  function emailValidator (event) {
+    const target = event.target;
+    if (target.validity.valueMissing || target.validity.typeMismatch || target.validity.patternMismatch){
+      target.classList.remove('is-valid');
+      target.classList.add('is-invalid');
+      return false;
+    }else if (target.validity.valid){
+      target.classList.remove('is-invalid');
+      target.classList.add('is-valid');
+      return true;
+    }
+  } 
+
+  country.addEventListener('input', function (e) {
+    countryValidator(e);
+  })
+  function countryValidator (event) {
+    const target = event.target;
+    if (target.validity.valueMissing || target.validity.tooShort || target.validity.tooLong){
+      target.classList.remove('is-valid');
+      target.classList.add('is-invalid');
+      return false;
+    }else if (target.validity.valid){
+      target.classList.remove('is-invalid');
+      target.classList.add('is-valid');
+      return true;
+    }
+  } 
+
+  zipCode.addEventListener('input', function (e) {
+    zipCodeValidator(e);
+  })
+  function zipCodeValidator (event) {
+    const target = event.target;
+    if (target.validity.valueMissing || target.validity.rangeOverflow || target.validity.rangeUnderflow){
+      target.classList.remove('is-valid');
+      target.classList.add('is-invalid');
+      return false;
+    }else if (target.validity.valid){
+      target.classList.remove('is-invalid');
+      target.classList.add('is-valid');
+      return true;
+    }
+  } 
+
+  password.addEventListener('input', function (e) {
+    passwordValidator(e);
+  })
+  function passwordValidator (event) {
+    const target = event.target;
+    if (target.validity.valueMissing || target.validity.tooShort || target.validity.tooLong || target.validity.patternMismatch){
+      target.classList.remove('is-valid');
+      target.classList.add('is-invalid');
+      target.setCustomValidity('Password must be between 8 and 16 characters');
+      return false;
+    }
+    target.classList.remove('is-invalid');
+    target.classList.add('is-valid');
+    target.setCustomValidity('');
+    return true;
+  } 
+
+  passwordConfirmation.addEventListener('input', function (e) {
+    passwordConfirmationValidator(e);
+  })
+  function passwordConfirmationValidator (event) {
+    const target = event.target;
+    if (target.validity.valueMissing || target.validity.tooShort || target.validity.tooLong || target.validity.patternMismatch){
+      target.classList.remove('is-valid');
+      target.classList.add('is-invalid');
+      target.setCustomValidity('Password must be between 8 and 16 characters');
+      return false;
+    } else if (target.value !== password.value){
+      target.classList.remove('is-valid');
+      target.classList.add('is-invalid');
+      target.setCustomValidity('Your confirmation doesn\'t match with your password.');
+      return false
+    }
+    target.classList.remove('is-invalid');
+    target.classList.add('is-valid');
+    target.setCustomValidity('');
+    return true;
+  }
+})();
